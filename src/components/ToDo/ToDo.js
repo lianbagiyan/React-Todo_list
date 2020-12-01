8e22498967f191c1985fe8acd426478b25a34842
Liana <bagiyanliana2@gmail.com>
2020-12-01 21:43:46 +0400

    added delete for tasks

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,76 +1,120 @@
 import React, { Component } from 'react';
-import Task from '../Task/Task';
-import {Button} from 'react-bootstrap';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faTrash } from '@fortawesome/free-solid-svg-icons';
-// import idGenerator from '../../helpers/idGenerator';
+//import Task from './task/Task';
+import idGenerator from '../../helpers/idGenerator';
+import { Container, Row, Col, FormControl, InputGroup, Button, Card } from 'react-bootstrap';
+import styles from './todoStyle.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 class ToDo extends Component {
-    constructor(props){
-        super(props);
+    state = {
+        tasks: [],
+        inputValue: ''
+    };
 
-        this.state = {
-            tasks: [],
-            inputVal: ''
-        }
-    }
-
-    handleChange = (event) => {
+    handleInputChange = (event) => {
         this.setState({
-            inputVal: event.target.value
+            inputValue: event.target.value
         });
-    }
+    };
 
-    handleKeyDown=(event)=> {
+    handleKeyDown = (event)=>{
         if(event.key === 'Enter'){
             this.addTask();
         }
-    }
+    };
 
-    addTask = () => {
-        const {inputVal} = this.state;
-        if(!inputVal){
+    addTask = ()=>{
+        const {inputValue} = this.state;
+        if(!inputValue){
             return;
         }
 
-        // const newTask = {
-        //     text: inputVal,
-        //     _id: idGenerator()
-        // }
+        const newTask = {
+            text: inputValue,
+            _id: idGenerator()
+        };
 
-        const tasks = [...this.state.tasks];
-
-        tasks.push(inputVal);
-
+        const tasks = [newTask, ...this.state.tasks];
         this.setState({
             tasks: tasks,
-            inputVal: ''
-        })
-    }
+            inputValue: ''
+        });
+    };
 
-    render(){
+    removeTask = (taskId)=>{
+        const newTasks = this.state.tasks.filter(task => task._id !== taskId);
+        this.setState({
+            tasks: newTasks
+        });
+    };
+
+    render() {
+        const {tasks, inputValue} = this.state;
+        const tasksArray = tasks.map((task) => {
+            return (
+                <Col key={task._id} xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <Card className={styles.task}>
+                        <Card.Body>
+                            <Card.Title>{task.text}</Card.Title>
+                            <Card.Text>
+                                {task.text}
+                            </Card.Text>
+                            <Button variant="warning" className={styles.actionButton}>
+                            <FontAwesomeIcon icon={faEdit} />
+                            </Button>
+
+                            <Button 
+                            variant="danger"  
+                            className={styles.actionButton}
+                            onClick = {()=>this.removeTask(task._id)}
+                            >
+                            <FontAwesomeIcon icon={faTrash} />
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            )
+        });
+
+
         return (
-            <div className='taskContainer'>
-                <input 
-                    type="text" 
-                    placeholder='write smth..' 
-                    onKeyDown={this.handleKeyDown}
-                    onChange={this.handleChange}
-                    value={this.state.inputVal}
-                />
-                <Button  
-                    variant="danger"
-                    onClick={this.addTask}
-                    disabled={!this.state.inputVal}
-                >Add task</Button>
-                <ol>
-                {this.state.tasks.map((item, index)=>{
-                    return <Task data={item} key={index} />
-                })}
-                </ol>
+            <div className={styles.toDo}>
+                <Container>
+                    <Row className='justify-content-center'>
+                        <Col sm={10} xs={12} md={8} lg={6}>
+                            <InputGroup className={styles.input}>
+                                <FormControl
+                                    placeholder="Input new task"
+                                    aria-label="Input new task"
+                                    aria-describedby="basic-addon2"
+                                    onChange={this.handleInputChange}
+                                    onKeyDown = {this.handleKeyDown}
+                                    value = {inputValue}
+                                />
+                                <InputGroup.Append>
+                                    <Button 
+                                    variant="primary"
+                                    onClick={this.addTask}
+                                    disabled = {!inputValue}
+                                    >
+                                    Add
+                                    </Button>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </Col>
+
+                    </Row>
+
+                    <Row>
+                        {tasksArray}
+                    </Row>
+                </Container>
+
             </div>
         );
-    }
+    };
+
 }
 
 export default ToDo;
