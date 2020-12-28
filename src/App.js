88679d09335ff10041bf9fb27c7a4512c9d76930
Liana <bagiyanliana2@gmail.com>
2020-12-28 20:24:52 +0400

    added react-router, used routes for menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Product from './components/Product/Product';
-import ToDo from './components/ToDo/ToDo';
+import ToDo from './components/pages/ToDo/ToDo';
+import About from './components/pages/About/About';
+import SingleTask from './components/pages/SingleTask/SingleTask';
+import NotFound from './components/pages/NotFound/NotFound';
+import NavMenu from './components/NavMenu/NavMenu';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
+
+  const routes = [
+{
+  path: '/',
+  component: ToDo
+},
+
+{
+  path: '/demo'
+},
+{
+  path: '/task',
+  component: ToDo
+},
+{
+  path: '/about',
+  component: About
+},
+{
+  path: '/task/:id',
+  component: SingleTask
+},
+{
+  path: '/404',
+  component: NotFound
+}
+  ];
+
   return (
-    <>
-      <Product 
-        name='apple' 
-        price='35$' 
-        description='Fresh apple' 
-      />
-      <ToDo />
-    </>
+    <div className="App">
+      <NavMenu />
+
+      <Switch>
+      {  routes.map((item, index) => 
+        <Route 
+          path={item.path}
+         exact 
+         component={item.component} 
+          key={index}
+         />) }
+      <Redirect to='/404' />
+    </Switch>
+
+    </div>
+
   );
 }
 
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -5,68 +5,73 @@ import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 import styles from './task.modules.css';
 import PropTypes from 'prop-types';
 import {formatDate} from '../../helpers/utils';
+import { Link } from 'react-router-dom';
 
 class Task extends PureComponent{
-    state = {
-        checked: false
-    };
-    
-    handleCheck = ()=>{
-        this.setState({
-            checked: !this.state.checked
-        });
-    
-        const {onCheck, data} = this.props;
-        onCheck(data._id);
-    };
-    
-    
-        render() {
-            const task = this.props.data;
-            const {checked} = this.state;
-            const {disabled} = this.props;
-    
-            return (
-                <Card className={`${styles.task} ${checked ? styles.selected: ''}`}>
-                            <Card.Body>
-                                <input 
-                                type='checkbox' 
-                                onClick = {this.handleCheck}
-                                />
-                                <Card.Title>{task.title}</Card.Title>
-                                <Card.Text>Description: {task.description}</Card.Text>
-                                <Card.Text className={styles.date}>Date: {formatDate(task.date)}</Card.Text>
-                                <Card.Text className={styles.date}>Created at: {formatDate(task.created_at)}</Card.Text>
-    
-                                <Button 
-                                variant="warning" 
-                                className={styles.actionButton}
-                                disabled = {disabled}
-                                onClick = {()=> this.props.onEdit(task)}
-                                >
-                                <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-    
-                                <Button 
-                                variant="danger"  
-                                className={styles.actionButton}
-                                onClick = {()=>this.props.onRemove(task._id)}
-                                disabled = {disabled}
-                                >
-                                <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </Card.Body>
-                        </Card>
-            );
-        }
-    
+state = {
+    checked: false
+};
+
+handleCheck = ()=>{
+    this.setState({
+        checked: !this.state.checked
+    });
+
+    const {onCheck, data} = this.props;
+    onCheck(data._id);
+};
+
+
+    render() {
+        const task = this.props.data;
+        const {checked} = this.state;
+        const {disabled} = this.props;
+        
+        return (
+            <Card className={`${styles.task} ${checked ? styles.selected: ''}`}>
+                        <Card.Body>
+                            <input 
+                            type='checkbox' 
+                            onClick = {this.handleCheck}
+                            />
+                            <Card.Title>
+                            <Link to={`/task/${task._id}`}>
+                            {task.title}
+                            </Link>
+                            </Card.Title>
+                            <Card.Text>Description: {task.description}</Card.Text>
+                            <Card.Text className={styles.date}>Date: {formatDate(task.date)}</Card.Text>
+                            <Card.Text className={styles.date}>Created at: {formatDate(task.created_at)}</Card.Text>
+
+                            <Button 
+                            variant="warning" 
+                            className={styles.actionButton}
+                            disabled = {disabled}
+                            onClick = {()=> this.props.onEdit(task)}
+                            >
+                            <FontAwesomeIcon icon={faEdit} />
+                            </Button>
+
+                            <Button 
+                            variant="danger"  
+                            className={styles.actionButton}
+                            onClick = {()=>this.props.onRemove(task._id)}
+                            disabled = {disabled}
+                            >
+                            <FontAwesomeIcon icon={faTrash} />
+                            </Button>
+                        </Card.Body>
+                    </Card>
+        );
     }
-    
-    Task.propTypes = {
-        data: PropTypes.object.isRequired,
-        onRemove: PropTypes.func.isRequired,
-        onCheck: PropTypes.func.isRequired,
-        disabled: PropTypes.bool.isRequired
-    };
-    
-    export default Task;
+
+}
+
+Task.propTypes = {
+    data: PropTypes.object.isRequired,
+    onRemove: PropTypes.func.isRequired,
+    onCheck: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired
+};
+
+export default Task;
